53be291da87c27b09897a7180ec220cd9935dcef
AloBordo1992 <122152325+AloBordo1992@users.noreply.github.com>
2023-09-08 21:52:00 +0000

    Deploying to gh-pages from @ AloBordo1992/goit-js-hw-06-2.0@06563ccbef596385b3ff3cc88b9e2223623d0ca2 🚀

---
+++ task-01.2f85a02f.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("#categories").querySelectorAll(".item");console.log(`Number of categories: ${e.length}`),e.forEach((e=>{console.log(`Category: ${e.querySelector("h2").textContent}`),console.log(`Elements: ${e.querySelectorAll("li").length}`)}));
+//# sourceMappingURL=task-01.2f85a02f.js.map
---
+++ task-01.c106c764.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("#categories").querySelectorAll(".item");console.log("Number of categories: ".concat(e.length)),e.forEach((function(e){console.log("Category: ".concat(e.querySelector("h2").textContent)),console.log("Elements: ".concat(e.querySelectorAll("li").length))}))}();
+//# sourceMappingURL=task-01.c106c764.js.map
