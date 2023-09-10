19b6d80f082ff13516f066746a9fed1eb4a69f21
AloBordo1992 <122152325+AloBordo1992@users.noreply.github.com>
2023-09-10 20:30:40 +0000

    Deploying to gh-pages from @ AloBordo1992/goit-js-hw-06-2.0@85faa9d128678eaab5d5998cd9a36f57ddf3d091 🚀

---
+++ task-06.84f51ec2.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("#validation-input");console.dir(t),t.addEventListener("blur",(()=>{const e=parseInt(t.getAttribute("data-length"));t.value.length===e?(t.classList.add("valid"),t.classList.remove("invalid")):(t.classList.add("invalid"),t.classList.remove("valid"))}));
+//# sourceMappingURL=task-06.84f51ec2.js.map
---
+++ task-06.cd734864.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("#validation-input");console.dir(t),t.addEventListener("blur",(function(){var a=parseInt(t.getAttribute("data-length"));t.value.length===a?(t.classList.add("valid"),t.classList.remove("invalid")):(t.classList.add("invalid"),t.classList.remove("valid"))}))}();
+//# sourceMappingURL=task-06.cd734864.js.map
