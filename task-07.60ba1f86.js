bf818e5d84137837e261fbf65ef1c694829c9b5b
AloBordo1992 <122152325+AloBordo1992@users.noreply.github.com>
2023-09-13 13:18:23 +0000

    Deploying to gh-pages from @ AloBordo1992/goit-js-hw-06-2.0@942ea004077d9cb6016b7acaedf611575403fdae 🚀

---
+++ task-07.60ba1f86.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("#font-size-control"),t=document.querySelector("#text");console.dir(e),e.addEventListener("input",(({currentTarget:e})=>t.style.fontSize=`${e.value}px`));
+//# sourceMappingURL=task-07.60ba1f86.js.map
---
+++ task-07.f389a9e7.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("#font-size-control"),t=document.querySelector("#text");console.dir(e),e.addEventListener("input",(function(e){var n=e.currentTarget;return t.style.fontSize="".concat(n.value,"px")}))}();
+//# sourceMappingURL=task-07.f389a9e7.js.map
