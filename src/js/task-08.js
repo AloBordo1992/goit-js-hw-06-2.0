393582cf54a865a301ad024c90a30382351d1bef
AloBordo1992 <122152325+AloBordo1992@users.noreply.github.com>
2023-09-13 20:40:45 +0200

    task 8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,20 @@
+const formLog = document.querySelector('.login-form');
 
+formLog.addEventListener('submit', onSubmit);
+
+function onSubmit(evt) {
+  evt.preventDefault();
+
+  const { email, password } = evt.currentTarget.elements;
+
+  if (!email.value.trim() || !password.value.trim()) {
+    return alert('Please fill in all the fields!');
+  } else {
+    const formData = {
+      email: email.value,
+      password: password.value,
+    };
+    console.log(formData);
+    e.currentTarget.reset();
+  }
+}
